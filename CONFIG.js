b29fa1562106853fe104dfe3215b4202319c664c
lliugang <981148612@qq.com>
2024-02-23 15:46:50 +0800

    1111

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "你好，我有件事想告诉你：做我的男/女朋友好吗？",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+    "请真诚地回答，如果你保持沉默并离开，那就表示你同意了。",
+  buttonYes: "我 同 意",
+  buttonNo: "我 不 同 意",
+  titleModar: "谢 谢 你",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "谢 谢 你 让 我 有 机 会 可 以 在 你 身 边，照 顾 和 保 护 你。 </br> 今 晚 我 会 去 接 你， 你 要 穿 得 漂 亮 点 哦 ！ 会 有 一 个 惊 喜 等 着 你。</br> Love you ",
 };
